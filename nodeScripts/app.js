e63600a73c5938f6c8ce8274f066dd1568a10701
uvg <nis16104@uvg.edu.gt>
2020-11-21 11:43:00 -0600

    python backround script

--- nodeScripts/app.js
+++ nodeScripts/app.js
@@ -61,11 +61,10 @@ app.post("/createvm", function(req, res) {
                 sudo wget -P /home/ https://raw.githubusercontent.com/nistalhelmuth/all-as-code/naive2/script.sh
                 sudo chmod +x /home/script.sh
                 sudo mkdir /home/logs
+                sudo chmod -R 777 /home/logs
                 sudo wget -P /home/ https://raw.githubusercontent.com/nistalhelmuth/all-as-code/naive2/serve.py
-                cd /home/
-                sudo python3 serve.py
+                sudo nohup python3 /home/serve.py &
                 sudo /home/script.sh nistalhelmuth/pipelines-sandbox > /home/logs/logs.txt
-                
               `
             },
           ]
