6dff9ef41fc1c6adbad2ef36918aa2e2648be625
uvg <nis16104@uvg.edu.gt>
2020-11-21 11:19:47 -0600

    update server

--- serve.py
+++ serve.py
@@ -1,17 +1,15 @@
 import http.server
 import socketserver
 
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.path = '/home/logs/logs.txt'
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
-
-# Create an object of the above class
-handler_object = MyHttpRequestHandler
-
 PORT = 8000
-my_server = socketserver.TCPServer(("", PORT), handler_object)
+DIRECTORY = "/home/logs/"
 
-# Star the server
-my_server.serve_forever()
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
